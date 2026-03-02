2170a6de7dfaafecf70aa5b085dd1c2afa4eb8d6
melvin-logdat-dev <melvin.logdat@codeandtheory.com>
2026-03-02 14:29:21 +0800

    Fixed base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/whimsical-whiskers/',
+  base: '/your-purrfect-companion/',
 })
