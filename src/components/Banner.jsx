5856f7bb75d2c3be929002fe5d7a7fbfd9714d57
melvin-logdat-dev <melvin.logdat@codeandtheory.com>
2026-03-02 14:00:48 +0800

    Added slider for each image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,13 @@
 import catImage from "../assets/calico-cat.png";
 import preloader from "../assets/loading-cat.gif";
 import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Banner = ({ breedId }) => {
-  const [breedImage, setBreedImage] = useState(null);
+  const [images, setImages] = useState([]);
   const [breedName, setBreedName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,7 +20,7 @@ const Banner = ({ breedId }) => {
         const [breedRes, imageRes] = await Promise.all([
           fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`),
           fetch(
-            `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+            `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&limit=5`,
           ),
         ]);
 
@@ -26,17 +30,16 @@ const Banner = ({ breedId }) => {
         setBreedName(breedData?.name || "Cat");
 
         if (imageData?.length > 0) {
-          const url = imageData[0]?.url;
-
-          const img = new Image();
-          img.src = url;
-          img.onload = () => {
-            setBreedImage(url);
-            setIsLoading(false);
-          };
+          setImages(
+            imageData.map((img) => ({
+              src: img.url,
+              alt: breedData?.name || "Cat",
+            })),
+          );
         } else {
-          setIsLoading(false);
+          setImages([{ src: catImage, alt: "Cat Banner" }]);
         }
+        setIsLoading(false);
       } catch (err) {
         console.error("Error fetching banner data:", err);
         setIsLoading(false);
@@ -50,8 +53,23 @@ const Banner = ({ breedId }) => {
     <div className="bannerContainer">
       {isLoading ? (
         <img src={preloader} alt="preloader" className="preloader" />
-      ) : breedImage ? (
-        <img src={breedImage} alt={breedName} className="BannerImage" />
+      ) : images.length > 0 ? (
+        <>
+          <Swiper
+            modules={[Navigation, Autoplay]}
+            loop
+            autoplay={{ delay: 3000 }}
+            autoHeight={true}
+            spaceBetween={20}
+            navigation={true}
+          >
+            {images.map((img, i) => (
+              <SwiperSlide key={i}>
+                <img src={img.src} alt={img.alt} className="BannerImage" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
       ) : (
         <img src={catImage} alt="Cat Banner" className="BannerImage" />
       )}
